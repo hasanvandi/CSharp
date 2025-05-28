2d1ce91b7f7e08afca8ce793d5f2186f4cfab388
mahdi <mahdihasanvandi2540@gmail.com>
2025-05-28 13:14:37 +0330

    Add project files.

---
+++ Calculator/Method.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Method
+    {
+        public void Calculate(string name, int number1, int number2, string oprator, int result)
+        {
+            Console.WriteLine($"{name}:{number1} {oprator} {number2} = {result}");
+        }
+
+        public int GetNumber(string numberOrder)
+        {
+            Console.Write($"Please Enter {numberOrder} number:");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Method method = new Method();
+            int num01 = method.GetNumber("First");
+            int num02 = method.GetNumber("Secound");
+            Console.WriteLine();
+
+  
+            Console.WriteLine("sum[a,A,+]");
+            Console.WriteLine("sub[-, s, S]");
+            Console.WriteLine("mult[*,M,m]");
+            Console.WriteLine("Div[/,D,d]");
+            Console.WriteLine("Which one?");
+            string userInput = Console.ReadLine();
+
+
+            if (userInput == "+" || userInput == "a" || userInput == "A")
+            {
+                method.Calculate("Add", num01, num02, "+", num01 + num02);
+            }
+            else if (userInput == "-" || userInput == "S" || userInput == "s")
+            {
+                method.Calculate("Subtract", num01, num02, "-", num01 + num02);
+            }
+            else if (userInput == "*" || userInput == "M" || userInput == "m")
+            {
+                method.Calculate("Multiply", num01, num02, "*", num01 + num02);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the oprator correctly.");
+            }
+
+
+            Console.ReadLine();
+        }
+ 
+    }
+    
+}
