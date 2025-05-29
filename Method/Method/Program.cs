317ea35b39c82b0b4e43ff8ccd9384ce7637e752
mahdi <mahdihasanvandi2540@gmail.com>
2025-05-29 11:01:52 +0330

    method added

---
+++ Method/Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Multiple(5,20);
+  
+            Console.ReadLine();
+        }
+        //عدد دوم مضربی از عدد اول هست یا خیر
+        public static bool Multiple(int number1, int number2)
+        {
+            if (number2 % number1 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Method/Method2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isEven(8));
+            Console.ReadLine();
+        }
+        //زوج یا فرد بودن عدد 
+        public static bool isEven(int number)
+        {
+          if(number % 2 == 0)
+            {
+                return true;
+            }
+          return false;
+        }
+    }
+}
